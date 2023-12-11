328b23ce9f1626eb27fc7928a8af67c582baf42d
Gebremedhin <kahsay.gebremedhin@powercoders.org>
2023-12-11 20:44:02 +0100

    Update goConstant filr

--- go_variables/goConstant.go
+++ go_variables/goConstant.go
@@ -20,5 +20,19 @@ func main() {
 		constVar2 string = "Strings"
 		constVar3 bool = true
 	)
+	
 	fmt.Println(constVar, constVar1, constVar2, constVar3)
+	/*
+	var a int = 3
+	var c = a + constVar // invalid operation: constVar + a (mismatched types int32 and int) 
+	*/
+	const a = 5 + 7.5 // Valid 12.5
+	const b = 12/5    // Valid 2
+	const c = 'z' + 1 // Valid treated as rune (single quatation character)
+
+	//const d = "Hey" + true // Invalid (untyped string constant and untyped boolean constant are not compatible with each other)
+
+	fmt.Println(a, b, c)
+
+	
 }
